af228c8001303b31293a6ef105669a840793ca2f
RomanStul <154585152+RomanStul@users.noreply.github.com>
2023-12-30 16:06:11 +0100

    Add files via upload

---
+++ header_scan.py
@@ -0,0 +1,43 @@
+import re
+
+
+class SenderCounter:
+
+    def __init__(self):
+        # [appearance_count, score]
+        self.appearances = {}
+
+    def load_sender(self, headers_dict, is_ok):
+        sender_address = SenderCounter.find_mail(headers_dict)
+        if sender_address not in self.appearances:
+            self.appearances[sender_address] = [1, 0]
+        else:
+            self.appearances[sender_address][0] += 1
+        self.appearances[sender_address][1] += {True: 1, False: -1}[is_ok]
+
+    def result(self):
+        result = {}
+        for key in self.appearances.keys():
+            result[key] = self.appearances[key][1]/self.appearances[key][0]
+        return result
+
+    def test_sender(self, headers_dict):
+        sender_address = SenderCounter.find_mail(headers_dict)
+        value = self.appearances.get(sender_address, [1, 0])
+        return value[1]/value[0]
+
+    @staticmethod
+    def find_mail(headers_dict):
+        sender_address = re.search(r'(([\w.?+-]+)|([\w.?+-]*"[\w.?@+-]+"[\w.?+-]*))@(([\w-]+\.[\w.-]+)|(\[[0-9]+\]))', headers_dict["From"])
+
+        if sender_address is None:
+            return None
+
+        sender_address = sender_address.group()
+        return sender_address
+
+
+
+
+
+
